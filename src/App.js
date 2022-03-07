72345d62acd18d0fab3246132351c6df9f497ce2
Sc0tticus <ssinger303@gmail.com>
2022-03-06 20:54:42 -0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Box, List, Tag, ListItem, Divider } from '@chakra-ui/core';
+import { Pagination } from '@material-ui/lab';
+import usePagination from './Pagination';
+import { default as data } from './MOCK_DATA.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	let [page, setPage] = useState(1);
+	const PER_PAGE = 2;
+
+	const count = Math.ceil(data.length / PER_PAGE);
+	const _DATA = usePagination(data, PER_PAGE);
+
+	const handleChange = (e, p) => {
+		setPage(p);
+		_DATA.jump(p);
+	};
+
+	return (
+		<Box p="5">
+			<Pagination
+				count={count}
+				size="large"
+				page={page}
+				variant="outlined"
+				shape="rounded"
+				onChange={handleChange}
+			/>
+
+			<List p="10" pt="3" spacing={2}>
+				{_DATA.currentData().map(v => {
+					return (
+						<ListItem key={v.id} listStyleType="disc">
+							<span>{v.sku}</span> <Divider display="inline" orientation="vertical" />
+							<span> {v.category_type}</span> <Divider display="inline" orientation="vertical" />
+							<span>
+								<Tag color="#0f4211">${v.msrp}</Tag>
+							</span>
+						</ListItem>
+					);
+				})}
+			</List>
+
+			<Pagination
+				count={count}
+				size="large"
+				page={page}
+				variant="outlined"
+				shape="rounded"
+				onChange={handleChange}
+			/>
+		</Box>
+	);
 }
-
-export default App;
